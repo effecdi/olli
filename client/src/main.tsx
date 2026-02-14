e08f44fe5820e3a8a5d15b9ccad7dd9b7a41c1e7
bbo-di <ler01260721@gmail.com>
2026-02-14 19:19:47 +0900

    파비콘: 런타임에서 새 로고(@assets/logo.png)로 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import logoUrl from "@assets/logo.png";
+
+const ensureFavicon = () => {
+  const head = document.head;
+  let link = head.querySelector<HTMLLinkElement>('link[rel="icon"]');
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    link.type = "image/png";
+    head.appendChild(link);
+  }
+  link.href = logoUrl;
+};
+
+ensureFavicon();
 
 createRoot(document.getElementById("root")!).render(<App />);
